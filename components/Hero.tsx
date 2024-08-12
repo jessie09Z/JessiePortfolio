cc1e6d44ff6dc99e502b9bbfe1fa366a84971abf
Jessie <jessay0901@gmail.com>
2024-08-12 15:25:12 +1000

    init my portfolio

---
+++ components/Hero.tsx
@@ -0,0 +1,14 @@
+// primary section to see
+import React  from "react"
+const Hero =()=>{
+    return (
+       <div>
+        <h1>
+            Hello
+        </h1>
+       </div> 
+    )
+    
+}
+
+export default Hero;
