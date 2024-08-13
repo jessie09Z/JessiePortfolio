ede9062738928433a2630bab4d800938d54a6630
Jessie <jessay0901@gmail.com>
2024-08-13 10:00:15 +1000

    fix background

---
+++ components/RecentProjects.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const RecentProjects = () => {
+  return (
+    <div className='py-20 '>
+      <h1 className='heading '>
+        A summary of {" "} 
+        <span className='text-purple'> Jessie's recent projects</span>
+      </h1>
+    </div>
+  )
+}
+
+export default RecentProjects
